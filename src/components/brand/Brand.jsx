a8f8f262d807854e91711dfa389c2aabfc719818
ScottNiblock <scottniblock16@gmail.com>
2024-06-25 16:08:11 -0500

    Implemented brand component

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import './brand.css'
+import slack from '../../assets/slack.png'
+import shopify from '../../assets/shopify.png'
+import google from '../../assets/google.png'
+import dropbox from '../../assets/dropbox.png'
+import atlassian from '../../assets/atlassian.png'
 
 const Brand = () => {
   return (
-    <div>
-      Brand
+    <div className='gpt3__brand'>
+      <div className='gpt3__brand-images'>
+        <img src={google} alt='google' />
+        <img src={shopify} alt='shopify' />
+        <img src={slack} alt='slack' />
+        <img src={dropbox} alt='dropbox' />
+        <img src={atlassian} alt='atlassian' />
+      </div>
     </div>
   )
 }
